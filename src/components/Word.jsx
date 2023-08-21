6800ba7000860018019d7c2304dccb7a3d372991
Scott Winiarski <Scott1w1@hotmail.com>
2023-08-21 09:38:23 -0500

    1.08 Commit

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -8,9 +8,13 @@ class Word extends Component {
     renderColor(color) {
         return (color === undefined || color === null ? 'black' : color);
     }
+    handleClick() {
+        console.log('word clicked');
+      }
+
     render() { 
         return ( 
-            <div className='Word' style={{ color:this.renderColor(this.state.color) }}>
+            <div onClick={ this.handleClick } className='Word' style={{ color:this.renderColor(this.state.color) }}>
             { this.state.word }
             </div>
         );
