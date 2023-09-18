78dad5da07c8d894f25c0edc2ad87c92bc4a78ee
Scott Winiarski <Scott1w1@hotmail.com>
2023-09-18 08:14:30 -0500

    6.01 and 6.02 Commit * installed Axios npm install axios * created axios endpoint using local url and function

--- src/App.js
+++ src/App.js
@@ -5,33 +5,41 @@ import Counter from './components/Counter';
 import NewWord from './components/NewWord';
 
 const App = () => {
+  const apiEndpoint = "https://localhost:7083/api/word";
   const [words, setWords] = useState([]);
 
   const handleDelete = (wordId) => {
-    //creates a new array, by filtering out any id that doesn't equal the one that's clicked.
-    const mutableWords = words.filter(w => w.id !== wordId);
-    // assigns to the newly created array.
-    setWords(mutableWords);
+    // //creates a new array, by filtering out any id that doesn't equal the one that's clicked.
+    // const mutableWords = words.filter(w => w.id !== wordId);
+    // // assigns to the newly created array.
+    // setWords(mutableWords);
+    console.log(`delete: ${wordId}`);
   }
 
   const handleAdd = (word, color) => {
     // const { words } = this.state;
-    const id = words.length === 0 ? 1 : Math.max(...words.map(word => word.id)) + 1;
-    const mutableWords = words.concat({ id: id, word: word, color: color });
-    setWords(mutableWords);
+    // const id = words.length === 0 ? 1 : Math.max(...words.map(word => word.id)) + 1;
+    // const mutableWords = words.concat({ id: id, word: word, color: color });
+    // setWords(mutableWords);
+    console.log(`word: ${word}, color: ${color}`);
   }
 
   // this is the functional equivalent to componentDidMount
   // The Array must be empty!
   useEffect(() => {
     // initial state data stored here
-    let mutableWords = [
-      { id: 1, word: 'banana', color: 'yellow' },
-      { id: 2, word: 'apple', color: 'red' },
-      { id: 3, word: 'lime', color: 'green' },
-      { id: 4, word: 'grape', color: 'purple' },
-    ];
-    setWords(mutableWords);
+    // let mutableWords = [
+    //   { id: 1, word: 'banana', color: 'yellow' },
+    //   { id: 2, word: 'apple', color: 'red' },
+    //   { id: 3, word: 'lime', color: 'green' },
+    //   { id: 4, word: 'grape', color: 'purple' },
+    // ];
+    // setWords(mutableWords);
+    async function fetchData() {
+      const { data: fetchedWords } = await axios.get(apiEndpoint);
+      setWords(fetchedWords);
+    }
+    fetchData();
   }, [])
 
 
